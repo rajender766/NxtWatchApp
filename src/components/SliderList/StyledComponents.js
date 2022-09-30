1550b5a82680da1e6a67b073095cf3fc00b390ae
rajender766 <111174888+rajender766@users.noreply.github.com>
2022-09-30 14:30:42 +0530

    export styles

--- src/components/SliderList/StyledComponents.js
+++ src/components/SliderList/StyledComponents.js
@@ -10,17 +10,4 @@ export const PageContainer = Styled.li`
    
 `
 
-export const ImageContainer = Styled.p`
-    display: flex;
-    align-items:center;
-    background-color:transparent;
-    margin:0px;
-   
-`
 
-export const Content = Styled.h1`
-    font-size:15px;
-    font-family:"Roboto";
-    margin:7px;
-    color: ${props => (props.textColor ? '#f8fafc' : '#475569')};
-`
